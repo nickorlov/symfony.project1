943d543f7ad6b47bce4842b3e49aa0cecc571343
nickorlov <neserag95@mail.ru>
2018-03-23 09:53:49 -0400

    car entity creating

---
+++ src/AppBundle/Entity/Car.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="car")
+ */
+class Car
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=100)
+     * @Assert\NotBlank()
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="text", length=100)
+     * @Assert\NotBlank()
+     */
+    private $numbers;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNumbers()
+    {
+        return $this->numbers;
+    }
+
+    /**
+     * @param string $numbers
+     */
+    public function setNumbers($numbers)
+    {
+        $this->numbers = $numbers;
+    }
+}
